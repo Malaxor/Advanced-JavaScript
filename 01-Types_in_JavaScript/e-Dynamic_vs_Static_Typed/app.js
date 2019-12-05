4aa8726172ccc3e39a214c463cf89bd490c12ff1
PHILIP COSTACHE <nogobaduk@gmail.com>
2019-12-05 17:26:25 -0500

    dynamic/static and strong/weak typed languages

---
+++ 01-Types_in_JavaScript/e-Dynamic_vs_Static_Typed/app.js
@@ -0,0 +1,11 @@
+// JavaScript is a dynamically typed language
+// this means that we don't have to specify the type of variable (number, boolean, string, null, undefined)
+let me = "Philip" 
+let age = 36;
+let single = true;
+// the data type stored in the variable is implicitly understood by the engine
+
+// In a statically typed language, the variable type has to be explicitly declared (ex: Java, C++)
+
+// Static typed languages usually have fewer bugs but are more laborious to implement
+// Dynamic typed languages are more flexible and allows for faster programming
---
+++ 01-Types_in_JavaScript/f-Strong_vs_Weak_Typed/app.js
@@ -0,0 +1,4 @@
+// JavaScript is a dynamic and weakly typed language
+let a = "hello";
+a + 17; // "hello17"
+// a weakly typed language performs type coercion 
