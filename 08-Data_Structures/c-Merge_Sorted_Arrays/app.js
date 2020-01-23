6e337204bccb42d743e718bb26f44d4b9b1cc53e
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-01-23 16:58:13 -0500

    merge sorted arrays version 2

--- 08-Data_Structures/c-Merge_Sorted_Arrays/app.js
+++ 08-Data_Structures/c-Merge_Sorted_Arrays/app.js
@@ -1,26 +1,34 @@
 function mergeSortedArr(arr1, arr2) {
    const result = [];
-   let counter = 0;
-   let index1 = 0;
-   let index2 = 0;
-   const sumLength = array1.length + array2.length;
+   const sumLength = arr1.length + arr2.length;
+   let pointer1 = 0;
+   let pointer2 = 0;
+   
+   for(let i = 0; i < sumLength; i++) {
 
-   while(counter < sumLength) {
-      const isArr1Empty = index1 >= arr1.length;
-      const isArr2Empty = index2 >= arr2.length;
-
-      if(!isArr1Empty && (isArr2Empty || arr1[index1] < array2[index2])) {
-         result[counter] = arr1[index1];
-         index1++;
+      if(arr1[pointer1] === undefined) {
+         result.push(arr2[pointer2]);
+         pointer2++;
+         continue;
+      }
+      if(arr2[pointer2] === undefined) {
+         result.push(arr1[pointer1]);
+         pointer1++;
+         continue;
+      }
+      
+      if(arr1[pointer1] < arr2[pointer2]) {
+         result.push(arr1[pointer1]);
+         pointer1++;
       }
       else {
-         result[counter] = arr2[index2];
-         index2++;
+         result.push(arr2[pointer2]);
+         pointer2++
       }
-      counter++;
-   } 
+   }
    return result;
 }
+
 const array1 = [0, 2, 3, 5, 90];
 const array2 = [1, 2, 8];
 
